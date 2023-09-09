ddc3ea79d9289464b0307f2351995be597d35c46
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-09-10 01:59:59 +0600

    api added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>React</h2>
+      <Countries/>
+      
+    </>
+  )
+}
+
+export default App
